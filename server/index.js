207c81c38d86955573f3331a7144bc2619ce3c4c
phucpham09 <phamhuuphuc4049@gmail.com>
2025-03-20 12:26:20 +0700

    fixing socket

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const http = require('http').Server(app)
 
 const socketIO = require('socket.io')(http, {
 	cors: {
-		origin: "http://localhost:3000"
+		origin: ["http://localhost:3000", "https://itss-app-frontend.onrender.com"]
 	}
 });
 
