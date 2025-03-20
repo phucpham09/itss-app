b5cb28d94cffa344826f655c2e889d54133d2171
phucpham09 <phamhuuphuc4049@gmail.com>
2025-03-20 10:16:31 +0700

    fixing new db string connection

--- server/index.js
+++ server/index.js
@@ -31,12 +31,10 @@ const { Pool } = require('pg')
 // })
 
 const pool = new Pool({
-	user: process.env.PG_USER,
-	host: process.env.PG_HOST,
-	database: process.env.PG_NAME,
-	password: process.env.PG_PASSWORD,
-	port: parseInt(process.env.PG_PORT),
-	ssl:true
+	connectionString: process.env.DATABASE_URL, 
+  ssl: {
+    rejectUnauthorized: false,
+  },
 })
 
 const connectToDatabase = () => {
